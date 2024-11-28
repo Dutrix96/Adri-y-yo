09b85d160f8e58ad8e30aac845e76ae8855df62e
Aguila <joseaguila3012@gmail.com>
2024-11-28 12:11:46 +0100

    cosa del main

---
+++ Ejercicio3.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+ //3º  Calcula el área y la circunferencia de un círculo cuyo radio se debe preguntar al usuario.
+    println("necesito que me indiques el radio para calcular la circunferencia y el area")
+    var radio= readln().toInt()
+    var circ=2*Math.PI*radio
+    var area=Math.PI*(radio*radio)
+    println("el area es $area y la circunferencia $circ")
+
+
+
+
+
+}
